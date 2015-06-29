77b47f80ea0532fd9765f1540060d0624e9f0a5d
karan chhabra <karan@streamoid.com>
2015-06-29 09:10:24 +0530

    first commit

--- www/piqitclient.js
+++ www/piqitclient.js
@@ -1,40 +1,22 @@
 var piqitclient = {
-    imageSearch: function(params,success,fail) {
-        navigator.notification.alert(
-              JSON.stringify(params),    // message
-              null,       // callback
-              "Workshop", // title
-              'OK'        // buttonName
-          );
-        var camQuality = params.quality? params.quality:75;
-        var camSourceType = params.sourceType ? params.sourceType:Camera.PictureSourceType.CAMERA;
-        var camAllowEdit = false;
-        
-        var options={quality:camQuality, destinationType: Camera.DestinationType.DATA_URL,
-        sourceType:camSourceType, allowEdit:camAllowEdit, encodingType: Camera.EncodingType.JPEG,
-        saveToPhotoAlbum: false};
-    navigator.notification.alert(
-              JSON.stringify(options),    // message
-              null,       // callback
-              "Workshop", // title
-              'OK'        // buttonName
-          );
-        navigator.camera.getPicture(function(imageData){
-            piqitclient.onPhotoDataSuccess(imageData,success,fail);
+    imageSearch: function(params, success, fail) {
+        var camQuality = params.quality ? params.quality : 75;
+        var camSourceType = params.sourceType ? params.sourceType : Camera.PictureSourceType.CAMERA;
+        var camAllowEdit = params.allowEdit ? true : false;
+
+        var options = {quality: camQuality, destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: camSourceType, allowEdit: camAllowEdit, encodingType: Camera.EncodingType.JPEG,
+            saveToPhotoAlbum: false};
+
+        navigator.camera.getPicture(function(imageData) {
+            piqitclient.onPhotoDataSuccess(imageData, success, fail);
         }
-        ,function(){
-            navigator.notification.alert(
-              "imageDatafail",    // message
-              null,       // callback
-              "Workshop", // title
-              'OK'        // buttonName
-          );
-            //piqitclient.onFail(fail);
+        , function() {
+            piqitclient.onFail(fail);
         }
-        ,options);
+        , options);
     },
-    onPhotoDataSuccess: function(imageData, successCallback,failureCallback) {
-    
+    onPhotoDataSuccess: function(imageData, successCallback, failureCallback) {
         $.ajax({
             type: "POST",
             url: "http://staging.streamoid.com/demo/cordovaApi.php",
@@ -47,7 +29,6 @@ var piqitclient = {
         });
     },
     onFail: function(failureCallback) {
-        
         failureCallback();
     }
 };
